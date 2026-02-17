abf96bfcc1c5a3d7ed00c82b2e3b1c4a18d1274b
Chrisser <164577349+cskhomo@users.noreply.github.com>
2026-02-17 15:51:07 +0200

    fill baord with 1...9

--- tic-tac-toe/board.cpp
+++ tic-tac-toe/board.cpp
@@ -4,19 +4,19 @@
 using namespace std;
 
 Board::Board(){
-    for (int i = 0; i < size; i++){
-        for (int i = 0; i < size; i++){
-            cells[i][i] = 0; 
+
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            cells[i][j] = (i * size) + (j + 1);
         }
     }
-
 }
 
 void Board::draw(){
 
     for (int i = 0; i < size; i++){
-        for (int i = 0; i < size; i++){
-            cout << cells[i][i] << " ";
+        for (int j = 0; j < size; j++){
+            cout << cells[i][j] << " ";
         }
         cout << endl;
     }
